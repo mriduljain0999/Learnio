5539d017a23933eecce47e587ad4c80a9b6f9ca6
Mridul <127869481+mriduljain0999@users.noreply.github.com>
2024-11-09 19:31:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://learnio-api.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
